66578c567f4a6aafad2d875ec3d0d2eba4de2a79
kazuki mokuo <sp9t5ns9@icloud.com>
2019-09-08 11:07:10 +0900

    編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,5 @@ protected
 def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:profile])
   devise_parameter_sanitizer.permit(:account_update, keys: [:profile,:name])
-  
 end
 end
